1b2784d697b0b512b873f694a29906c90b9a501c
chriszc97 <czeas1997@gmail.com>
2022-02-07 15:12:41 -0500

    dependecies installed and DB created

---
+++ models/comment.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Comment.belongsTo(models.Post, { foreignKey: 'postId' });
+      Comment.belonfsTo(models.Post, { foreignKey: 'postId' });
+    }
+  }
+  Comment.init(
+    {
+      body: DataTypes.STRING,
+      postId: DataTypes.INTEGER,
+      UserId: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Comment',
+      tableName: 'comments'
+    }
+  );
+  return Comment;
+};
