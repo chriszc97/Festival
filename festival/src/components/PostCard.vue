f8bfd8e7dc08b58b8cd8800ce95cb16bac994872
chriszc97 <czeas1997@gmail.com>
2022-02-10 11:50:45 -0500

    home page showing posts

---
+++ festival/src/components/PostCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="post" >   
+      <h2>{{post.title}} </h2> 
+      <p>{{post.content}} </p>
+      <img :src='post.img' alt="" />
+      <h4>Likes: {{post.likes}}</h4>
+    </div>
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'PostCard',
+  props: ['post'],
+}
+</script>
+
+
+<style >
+h2{
+  margin: 10px;
+  font-size: 30px;
+  padding: 0;
+  color: rgb(1, 10, 9);
+}
+
+
+img{
+  /* max-width: 400px;
+  max-height: 480px; */
+  margin: auto;
+  /* border-bottom: 1px solid rgba(255, 255, 255, 0.2); */
+  padding:  0;
+}
+
+</style>
+
+
+
+
--- festival/src/pages/Home.vue
+++ festival/src/pages/Home.vue
@@ -1,25 +1,34 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <PostCard :post='post' :key='post.id' v-for='post in posts' />
 
   </div>
 </template>
 
-<script>
-// import axis from 'axios'
+<script> 
+import axios from 'axios'
+import PostCard from '../components/PostCard.vue'
+const BASE_URL = 'http://localhost:3001/api'
 
 export default {
   name: 'Home',
   components: {
-
+    PostCard
   },
   data:() =>({
+    posts:{}
 
   }),
   mounted(){
+    this.getPosts()
 
   },
   methods:{
+    async getPosts(){
+      const result = await axios.get(`${BASE_URL}/post`)
+      this.posts = result.data
+    }
 
   }
 
