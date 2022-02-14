1c9dbdc4d815f5eab33004ba27c4b05b01a26ac1
chriszc97 <czeas1997@gmail.com>
2022-02-14 11:51:20 -0500

    added another controller to comments model

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -7,6 +7,17 @@ const createComment = async (req, res) => {
     res.send(comment);
   } catch (error) {}
 };
+const getComment = async (req, res) => {
+  try {
+    let id = parseInt(req.params.id);
+    const result = await Comment.findAll({
+      where: { postId: id }
+    });
+    res.send(result);
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   createComment
